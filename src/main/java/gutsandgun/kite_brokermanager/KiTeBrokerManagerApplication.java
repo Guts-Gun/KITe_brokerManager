423987712e90babce67d5b37b68cafc707a49b21
YunJae Park <zerglisk123@naver.com>
2023-01-10 00:29:57 +0900

    create

---
+++ src/main/java/gutsandgun/kite_brokermanager/KiTeBrokerManagerApplication.java
@@ -0,0 +1,13 @@
+package gutsandgun.kite_brokermanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiTeBrokerManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KiTeBrokerManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/gutsandgun/kite_brokermanager/KiTeBrokerManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package gutsandgun.kite_brokermanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiTeBrokerManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
