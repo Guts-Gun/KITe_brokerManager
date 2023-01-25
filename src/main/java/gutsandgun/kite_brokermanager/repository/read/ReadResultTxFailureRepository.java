9960cc83adbfa6479cc5e3e8d05b3d4d3d87dbb9
Karla <karlako9482@gmail.com>
2023-01-26 06:02:06 +0900

    [CHORE] DB 설정 수정

--- src/main/java/gutsandgun/kite_brokermanager/repository/read/ReadResultTxFailureRepository.java
+++ src/main/java/gutsandgun/kite_brokermanager/repository/read/ReadResultTxFailureRepository.java
@@ -1,9 +1,9 @@
 package gutsandgun.kite_brokermanager.repository.read;
 
-import gutsandgun.kite_brokermanager.entity.read.LogFailure;
+import gutsandgun.kite_brokermanager.entity.read.ResultTxFailure;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReadLogFailureRepository extends JpaRepository<LogFailure, Long> {
+public interface ReadResultTxFailureRepository extends JpaRepository<ResultTxFailure, Long> {
 }
